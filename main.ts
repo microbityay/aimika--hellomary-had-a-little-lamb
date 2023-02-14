7e337cbebb690461f9a8f413b7e6e9ca8254b406
microbityay <124930299+microbityay@users.noreply.github.com>
2023-02-13 16:55:46 -0800

    Completed AImika

--- main.ts
+++ main.ts
@@ -1,38 +1,81 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . #
+        . # # # .
+        . . . . .
+        `)
+})
 input.onButtonPressed(Button.A, function () {
+    basic.showString("Sing the ABC's With Me")
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . #
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(1000)
     music.playTone(262, music.beat(BeatFraction.Whole))
-    music.playTone(466, music.beat(BeatFraction.Whole))
-    music.playTone(311, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(311, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
     music.playTone(262, music.beat(BeatFraction.Whole))
-    music.playTone(466, music.beat(BeatFraction.Whole))
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    music.playTone(466, music.beat(BeatFraction.Whole))
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    music.playTone(311, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(311, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    music.playTone(466, music.beat(BeatFraction.Whole))
-    music.playTone(466, music.beat(BeatFraction.Whole))
-    music.playTone(440, music.beat(BeatFraction.Whole))
-    music.playTone(466, music.beat(BeatFraction.Whole))
-    music.playTone(311, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    music.playTone(466, music.beat(BeatFraction.Whole))
-    music.playTone(415, music.beat(BeatFraction.Whole))
-    music.playTone(466, music.beat(BeatFraction.Whole))
-    music.playTone(415, music.beat(BeatFraction.Whole))
     music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Breve))
+})
+input.onGesture(Gesture.FreeFall, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        # . . . #
+        . . . . .
+        `)
+    music.playMelody("C5 C5 C5 C5 C5 - - - ", 66)
+})
+input.onSound(DetectedSound.Loud, function () {
+    basic.showString("Shh! We need to be quite")
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showString("Hello! My name is AImika!")
 })
 input.onButtonPressed(Button.B, function () {
+    basic.showString("Guess this tune:")
     music.playTone(659, music.beat(BeatFraction.Whole))
     music.playTone(587, music.beat(BeatFraction.Whole))
     music.playTone(523, music.beat(BeatFraction.Whole))
@@ -60,6 +103,10 @@ input.onButtonPressed(Button.B, function () {
     music.playTone(587, music.beat(BeatFraction.Whole))
     music.playTone(523, music.beat(BeatFraction.Whole))
 })
-basic.forever(function () {
-	
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showString("Mera naam AImika hain")
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("Ahhhh!!")
+    music.startMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once)
 })
